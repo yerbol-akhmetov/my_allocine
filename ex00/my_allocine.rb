f4d0167dbf24485e4e74c559be8bd133e9942da9
yerbol akhmetov <akhmetov_y@qwasar.io>
2022-07-25 12:17:09 +0000

    Save1

---
+++ ex00/my_allocine.rb
@@ -0,0 +1,18 @@
+requests['Display all actors'] = "SELECT * FROM actors;"
+requests['Display all genres'] = "SELECT * FROM genres;"
+requests['Display the name and year of the movies'] = "SELECT mov_title, mov_year FROM movies;"
+requests['Display reviewer name from the reviews table'] = "SELECT rev_name FROM reviews;"
+
+requests["Find the year when the movie American Beauty released"] = ""
+requests["Find the movie which was released in the year 1999"] = ""
+requests["Find the movie which was released before 1998"] = ""
+requests["Find the name of all reviewers who have rated 7 or more stars to their rating order by reviews rev_name (it might have duplicated names :-))"] = ""
+requests["Find the titles of the movies with ID 905, 907, 917"] = ""
+requests["Find the list of those movies with year and ID which include the words Boogie Nights"] = ""
+requests["Find the ID number for the actor whose first name is 'Woody' and the last name is 'Allen'"] = ""
+
+requests["Find the actors with all information who played a role in the movies 'Annie Hall'"] = ""
+requests["Find the first and last names of all the actors who were cast in the movies 'Annie Hall', and the roles they played in that production"] = ""
+
+requests["Find the name of movie and director who directed a movies that casted a role as Sean Maguire"] = ""
+requests["Find all the actors who have not acted in any movie between 1990 and 2000"] = ""
