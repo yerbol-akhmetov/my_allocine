6a9049e777be5cf9f8d258c19fdbf81c4050f9b1
yerbol akhmetov <akhmetov_y@qwasar.io>
2022-07-26 11:40:50 +0000

    Savedd

--- ex00/my_allocine.rb
+++ ex00/my_allocine.rb
@@ -7,9 +7,9 @@ requests["Find the year when the movie American Beauty released"] = "SELECT mov_
 requests["Find the movie which was released in the year 1999"] = "SELECT mov_title FROM movies WHERE mov_year = '1999';"
 requests["Find the movie which was released before 1998"] = "SELECT mov_title FROM movies WHERE mov_year < '1998';"
 requests["Find the name of all reviewers who have rated 7 or more stars to their rating order by reviews rev_name (it might have duplicated names :-))"] = "SELECT rev_name FROM reviews WHERE id in (SELECT rev_id FROM movies_ratings_reviews WHERE rev_stars >= '7') order BY rev_name ASC;"
-requests["Find the titles of the movies with ID 905, 907, 917"] = "SELECT mov_title FROM movies WHERE id in (905, 907, 917);"
-requests["Find the list of those movies with year and ID which include the words Boogie Nights"] = ""
-requests["Find the ID number for the actor whose first name is 'Woody' and the last name is 'Allen'"] = ""
+requests["Find the titles of the movies with ID 905, 907, 917"] = "SELECT mov_title FROM movies WHERE id in (905,907,917);"
+requests["Find the list of those movies with year and ID which include the words Boogie Nights"] = "SELECT mov_year, id FROM movies WHERE mov_title LIKE '%Boogie Nights%';"
+requests["Find the ID number for the actor whose first name is 'Woody' and the last name is 'Allen'"] = "SELECT id FROM actors WHERE act_fname = 'Woody' AND act_lname = 'Allen';"
 
 requests["Find the actors with all information who played a role in the movies 'Annie Hall'"] = ""
 requests["Find the first and last names of all the actors who were cast in the movies 'Annie Hall', and the roles they played in that production"] = ""
