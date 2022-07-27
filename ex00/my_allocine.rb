f4bea589e323ad38991b50b45e60aa9846a2103a
yerbol akhmetov <akhmetov_y@qwasar.io>
2022-07-27 17:35:50 +0000

    Saveeee

--- ex00/my_allocine.rb
+++ ex00/my_allocine.rb
@@ -14,5 +14,5 @@ requests["Find the ID number for the actor whose first name is 'Woody' and the l
 requests["Find the actors with all information who played a role in the movies 'Annie Hall'"] = "SELECT * FROM actors WHERE id = (SELECT act_id FROM movies INNER JOIN movies_actors ON movies.id = movies_actors.mov_id WHERE mov_title = 'Annie Hall');"
 requests["Find the first and last names of all the actors who were cast in the movies 'Annie Hall', and the roles they played in that production"] = "SELECT act_fname, act_lname, role FROM actors INNER JOIN movies_actors ON actors.id = movies_actors.act_id WHERE id = (SELECT act_id FROM movies INNER JOIN movies_actors ON movies.id = movies_actors.mov_id WHERE mov_title = 'Annie Hall');"
 
-requests["Find the name of movie and director who directed a movies that casted a role as Sean Maguire"] = ""
+requests["Find the name of movie and director who directed a movies that casted a role as Sean Maguire"] = "SELECT f.dir_fname, f.dir_lname, m.mov_title FROM (SELECT * FROM directors AS d INNER JOIN directors_movies AS dm ON d.id = dm.dir_id WHERE dm.mov_id = (SELECT mov_id FROM movies_actors WHERE role = 'Sean Maguire')) AS f INNER JOIN movies as m ON f.mov_id = m.id;"
 requests["Find all the actors who have not acted in any movie between 1990 and 2000"] = ""
